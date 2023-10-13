33244c4b0bd021fc14d2ce0701562128d6814e1f
Yamil Mayta <gtr010yam@gmail.com>
2023-10-13 00:16:29 -0400

    Ultimos detalles

--- app/api/api.js
+++ app/api/api.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const API_URL = 'http://localhost:3000/api'
 
 export async function fetchColors() {
@@ -5,10 +7,55 @@ export async function fetchColors() {
         const response = await fetch(`${API_URL}/colors/`);
         if (!response.ok) {
             throw new Error('Error al obtener datos de la API');
-            
+
         }
         return await response.json();
     } catch (error) {
         throw new Error(error.message);
+    }
+}
+
+
+export async function fetchMessage() {
+    try {
+        const response = await fetch(`${API_URL}/messages/`);
+        if (!response.ok) {
+            throw new Error('Error al obtener las frases');
+        }
+        return await response.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+export async function postReaction(data) {
+    try {
+        const response = await fetch(`${API_URL}/reactions/createReaction`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+export async function getIpAddress() {
+    try {
+        const response = await axios.get('https://api.ipify.org?format=json');
+        const ipAddress = response.data.ip;
+        return ipAddress;
+    } catch (error) {
+        console.error('Error al obtener la dirección IP:', error);
+        return null;
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,145 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { DateTime } from 'luxon';
 
-import { fetchColors } from './api/api';
+import { fetchColors, fetchMessage, postReaction, getIpAddress } from './api/api';
+
+interface Color {
+  code: string,
+  name: string
+}
+interface Message {
+  message: string;
+}
+
 
 export default function Home() {
 
-  const [colors, setColors] = useState([]);
+  const [ip, setIp] = useState('');
+  const [date, setDate] = useState('');
+  const [reaction, setReaction] = useState(0);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [randomMessage, setRandomMessage] = useState([0]);
+  const [colors, setColors] = useState<Color[]>([]);
   const [randomColorIndexes, setRandomColorIndexes] = useState([0, 0]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
-    async function obtenerColores() {
+
+    async function fetchData() {
       try {
         const colores = await fetchColors();
-        console.log(colores);
+        const frases = await fetchMessage();
+        const ipAddress = await getIpAddress();
+        const boliviaDateTime = DateTime.now().setZone('America/La_Paz').toFormat('yyyy-MM-dd HH:mm:ss');
 
+        setDate(boliviaDateTime);
         setColors(colores);
+        setMessages(frases);
+        setIp(ipAddress);
+
+        if (colores.length > 0) {
+          const firstIndex = Math.floor(Math.random() * colores.length);
+          let secondIndex;
+          do {
+            secondIndex = Math.floor(Math.random() * colores.length);
+          } while (secondIndex === firstIndex);
+          setRandomColorIndexes([firstIndex, secondIndex]);
+
+        }
+
+        if (frases.length > 0) {
+          const randomMessageIndex = Math.floor(Math.random() * frases.length);
+          setRandomMessage([randomMessageIndex]);
+        }
+
       } catch (error) {
         console.log(error);
       }
     }
-    obtenerColores();
+    fetchData();
   }, []);
 
-  const getRandomColors = () => {
-    if (colors.length > 0) {
-      const firstIndex = Math.floor(Math.random() * colors.length);
-      let secondIndex;
-      do {
-        secondIndex = Math.floor(Math.random() * colors.length);
-      } while (secondIndex === firstIndex);
-      setRandomColorIndexes([firstIndex, secondIndex]);
-    }
-  };
 
-  useEffect(() => {
-    getRandomColors();
-  });
-
-
-  function getCurrentColorCode(index:any) {
+  function getCurrentColorCode(index: any) {
     if (colors.length > 0 && randomColorIndexes[index] !== null) {
       return colors[randomColorIndexes[index]].code;
     }
     return '';
   }
 
-  const backgroundColor = getCurrentColorCode(0);
-  const textColor = getCurrentColorCode(1);
+  function getCurrentMessage(index: any) {
+    if (messages.length > 0 && randomMessage[index] !== null) {
+      return messages[randomMessage[index]].message;
+    }
+    return '';
+  }
+
+  const handleLike = () => {
+    setReaction(1);
+    handleSubmit();
+  };
+
+  const handleDislike = () => {
+    setReaction(0);
+    handleSubmit();
+  };
+
+  const backgroundColorIndex = getCurrentColorCode(0);
+  const textColorIndex = getCurrentColorCode(1);
+  const messageRandom = getCurrentMessage(0);
+
+  const handleSubmit = async () => {
+
+    const data = {
+      ip: ip,
+      date: date,
+      textColor: textColorIndex,
+      textBackground: backgroundColorIndex,
+      message: messageRandom,
+      reaction: reaction,
+    };
+
+    try {
+      const response = await postReaction(data);
+      console.log('Datos enviados con éxito:', response);
+      setErrorMessage(null);
+    } catch (error) {
+      if (error instanceof Error) {
+        const errorObj = JSON.parse(error.message);
+        if (errorObj && errorObj.error) {
+          setErrorMessage(errorObj.error);
+        } else {
+          setErrorMessage('Error desconocido');
+        }
+        console.error('Error al enviar datos:', error);
+      } else {
+        console.error('Error desconocido:', error);
+      }
+    }
+  };
+
 
 
   return (
-    <div className="min-h-screen flex justify-center items-center" style={{ backgroundColor: backgroundColor }}>
+    <div className="min-h-screen flex justify-center items-center" style={{ backgroundColor: backgroundColorIndex }}>
       <main>
-        <h1 className="text-center text-4xl font-semibold" style={{ color: textColor }}>Hola mundo</h1>
-        <div className="flex justify-center items-center pt-20">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold p-7 rounded-full m-2 transition duration-300 ease-in-out transform hover:scale-105">
-            <FontAwesomeIcon icon={faThumbsUp} className="fa-lg w-10" />
+        {errorMessage ? (
+          <div className="error-message">
+            <h1 className="text-center text-xl bg-blue-500 text-white font-light p-10 rounded-full m-2">{errorMessage}</h1>
+          </div>
+        ) : (
+          <h1 className="text-center text-4xl font-semibold p-10" style={{ color: textColorIndex }}>{messageRandom}</h1>
+        )}
+        <div className="flex justify-center items-center">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold p-7 rounded-full m-2 transition duration-300 ease-in-out transform hover:scale-105" onClick={handleLike}>
+            <FontAwesomeIcon icon={faThumbsUp} className="fa-lg" />
           </button>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold p-7 rounded-full m-2 transition duration-300 ease-in-out transform hover:scale-105">
-            <FontAwesomeIcon icon={faThumbsDown} className="fa-lg w-10" />
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold p-7 rounded-full m-2 transition duration-300 ease-in-out transform hover:scale-105" onClick={handleDislike}>
+            <FontAwesomeIcon icon={faThumbsDown} className="fa-lg" />
           </button>
         </div>
       </main>
